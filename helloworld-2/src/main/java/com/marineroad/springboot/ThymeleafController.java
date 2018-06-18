d40702fa295a48953bedf0092b4af1f67eba3dae
Kohei Ogawa <40193657+ogawa3416@users.noreply.github.com>
2018-06-18 19:19:12 +0900

    2018/06/18 更新分

--- helloworld-2/src/main/java/com/marineroad/springboot/ThymeleafController.java
+++ helloworld-2/src/main/java/com/marineroad/springboot/ThymeleafController.java
@@ -20,7 +20,7 @@ public class ThymeleafController {
 		// 1.(1) 変数式
 		// ・コントローラーからテンプレートに値を渡す
 		// ・変数「msg」に値を設定
-		mav.addObject("msg", "コントローラーからテンプレートに値を渡す");
+		mav.addObject("msg", "コントローラーからテンプレートのここに値を渡す");
 
 		// 使用するビューを設定
 		mav.setViewName("thdemo"); // (3)
