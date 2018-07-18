47c5fbe8d316990f5de075e7fecc426201a21862
Kohei Ogawa <40193657+ogawa3416@users.noreply.github.com>
2018-07-18 18:48:36 +0900

    2018/07/18 更新分

---
+++ Java 勉強用/13章/src/ex13_01/Card.java
@@ -0,0 +1,6 @@
+package ex13_01;
+
+public class Card {
+	String suit;
+	int number;
+}
--- Java 勉強用/13章/src/ex13_01/Exec.java
+++ Java 勉強用/13章/src/ex13_01/Exec.java
@@ -2,6 +2,7 @@ package ex13_01;
 
 public class Exec {
 	public static void main(String[] args) {
+		Card card = new Card();
 
 	}
 
