7eddb0e0f27b678bebfe3fdc015e816c6d59da76
Kohei Ogawa <40193657+ogawa3416@users.noreply.github.com>
2018-08-08 17:27:26 +0900

    2018/08/08 更新分

---
+++ springbootapp/src/main/java/com/marineroad/springboot/IndexController.java
@@ -0,0 +1,14 @@
+package com.marineroad.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+  @RequestMapping(value = "/index", method = RequestMethod.GET)
+  String index() {
+    return "index";
+  }
+ 
+}
