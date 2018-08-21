311ef47be4995d4b62dbe2c6d5b09e1d02822e32
Kohei Ogawa <ogawa@oysterworld.jp>
2018-08-21 16:13:46 +0900

    2018/08/21 更新分

---
+++ oysterworld/src/main/java/com/example/demo/OysterworldApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OysterworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OysterworldApplication.class, args);
+	}
+}
---
+++ oysterworld/src/main/java/com/example/demo/ThymeleafController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ThymeleafController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/about")
+	public String about() {
+		return "about";
+	}
+
+	@RequestMapping("/services")
+	public String service() {
+		return "services";
+	}
+
+	@RequestMapping("/contact")
+	public String contact() {
+		return "contact";
+	}
+
+}
