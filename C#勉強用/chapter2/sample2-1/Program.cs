0ef4655b6f26341cec14c63a750506a53bc18507
Kohei Ogawa <ogawa@oysterworld.jp>
2018-08-23 14:20:34 +0900

    C#勉強のためのsampleコード

---
+++ C#勉強用/chapter2/sample2-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sample2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // フィートからメートルへの対応表を出力
+            for (int feet = 1; feet <= 10; feet++)
+            {
+                double meter = feet * 0.348;
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+    }
+}
