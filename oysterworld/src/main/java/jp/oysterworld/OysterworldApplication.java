bdba38dbb3511a5012ec0910045e5b521f3bcc3a
Kohei Ogawa <ogawa@oysterworld.jp>
2018-08-29 18:54:23 +0900

    オイスタ－ワールドのホームページ

--- oysterworld/src/main/java/jp/oysterworld/OysterworldApplication.java
+++ oysterworld/src/main/java/jp/oysterworld/OysterworldApplication.java
@@ -2,9 +2,16 @@ package jp.oysterworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OysterworldApplication {
+public class OysterworldApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OysterworldApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OysterworldApplication.class, args);
